2fb125d22b6626261bcc48db0eaafea22fb488d6
Jebb Burditt <jebb.burditt@gmail.com>
2024-11-26 19:07:18 -0800

    add testdata

---
+++ Tests/Model/TestData.cs
@@ -0,0 +1,5 @@
+﻿public static class TestData
+{
+    public static readonly Guid OwnerId = new Guid("386c8d9c-0a4c-e911-b80c-00505683fbf4");
+    public static readonly Guid CaseId = new Guid("7b1d36eb-0a3c-4893-b3db-0008d1316cfc");
+}
