04eeb9481a585b850e3e2a60b8422561d678275e
Jebb Burditt <jebb.burditt@gmail.com>
2024-12-25 12:22:06 -0800

    proof of concept for expression mapping dto to entity

---
+++ Tests/Integration/Database/AutoMapperExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using AutoMapper.Extensions.ExpressionMapping;
+using Shared.Database;
+using System.Linq;
+using System.Linq.Expressions;
+
+public class AutoMapperExtensionsTests(DatabaseContext context, IMapper mapper)
+{
+    [Fact]
+    public async Task TEST()
+    {
+        //ICollection<Payment> requests = await context.Vsd_PaymentSet.GetItemsAsync(mapper, r => r.Id > 0 && r.Id < 3, null);
+        //ICollection<Payment> users = await context.Vsd_PaymentSet.GetItemsAsync<Payment, Vsd_Payment>(mapper, u => u.Date < DateTime.Now);//, q => q.OrderBy(u => u.Name));
+        ICollection<Country> users = await context.Vsd_CountrySet.GetItemsAsync<Country, Vsd_Country>(mapper, u => u.Name != "Canada");//, q => q.OrderBy(u => u.Name));
+        //int count = await context.Request.Query<RequestDTO, Request, int, int>(mapper, q => q.Count(r => r.Id > 1));
+        Assert.NotNull(users);
+    }
+}
