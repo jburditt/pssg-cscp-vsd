4caba3ce608f699cc190f080a2d6da4bb420d026
Jebb Burditt <jebb.burditt@gmail.com>
2024-12-26 11:23:35 -0800

    remove automapper extension tests

--- Tests/Integration/Database/AutoMapperExtensionsTests.cs
+++
@@ -1,9 +0,0 @@
-﻿using AutoMapper;
-using AutoMapper.Extensions.ExpressionMapping;
-using Shared.Database;
-using System.Linq;
-using System.Linq.Expressions;
-
-public class AutoMapperExtensionsTests(DatabaseContext context, IMapper mapper)
-{
-}
